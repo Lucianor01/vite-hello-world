2a9976a11bd7a5fcc53cc3ee335b2be6b4814496
Lucianor01 <lucianorosario02@gmail.com>
2023-05-02 16:59:56 +0200

    Componente AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<!--! SCRIPT -->
+<script>
+
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+};
+
+</script>
+
+<!--! TEMPLATE -->
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+
+<!--! STYLE -->
+<style scoped></style>
